222a9b499b168eb85888a93f0a91a0d1d8f786f3
car <carmelogustavo@gmail.com>
2018-08-16 23:30:34 +0200

    feat: Calendar Module

---
+++ src/components/MyCalendar.js
@@ -0,0 +1,15 @@
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+
+const MyCalendar = props => (
+  <div>
+    <BigCalendar
+      startAccessor='startDate'
+      endAccessor='endDate'
+    />
+  </div>
+);
