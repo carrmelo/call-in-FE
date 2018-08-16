d2d1b6c284fe8fd2bc7d174f9830e3127b3b6ff4
car <carmelogustavo@gmail.com>
2018-08-16 21:21:49 +0200

    feat: handle inputs

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,19 +2,71 @@ import React, { Component } from 'react';
 import "./Create.css"
 
 class Create extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      description: '',
+      startDate: null,
+      startTime: null,
+      endDate: null,
+      endTime: null
+    };
+  }
+
+  handleInputChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    console.log(value, 'IIIIIII', name);
+    
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <form className="create_form_container">
-        <label>Title: </label>
-        <input type="text" placeholder="Title" />
-        <label>Description: </label>
-        <input type="text" placeholder="Description" />
-        <label>Start: </label>
-        <input type="date" />
-        <input type="time" />
-        <label>End: </label>
-        <input type="date" />
-        <input type="time" />
+        <label>Title:
+          <input
+            name="title"
+            type="text"
+            placeholder="Title"
+            onChange={this.handleInputChange} />
+        </label>
+        <label>Description:
+          <input
+            name="description"
+            type="text"
+            placeholder="Description"
+            onChange={this.handleInputChange} />
+        </label>
+        <label>Start:
+          <input
+            name="startDate"
+            type="date"
+            onChange={this.handleInputChange} />
+          <input
+            name="startTime"
+            type="time"
+            onChange={this.handleInputChange} />
+        </label>
+        <label>End:
+          <input
+            name="endDate"
+            type="date"
+            onChange={this.handleInputChange} />
+          <input
+            name="endTime"
+            type="time"
+            onChange={this.handleInputChange} />
+        </label>
+        <button onClick={this.handleClick}>Submit</button>
       </form>
     )
   }
