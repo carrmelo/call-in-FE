c494f9b758b13f049f9a1442f0211fee6edd184b
car <carmelogustavo@gmail.com>
2018-08-19 12:43:24 +0200

    feat: declare actions eventStore

--- src/stores/eventStore.js
+++ src/stores/eventStore.js
@@ -1,13 +1,57 @@
 import { observable, action } from 'mobx';
 
-export class Event {
+import calendarStore from './calendarStore';
 
-  @observable _id = "";
-  @observable title = "";
-  @observable description = "";
+class Event {
+  @observable _id = '';
+  @observable title = '';
+  @observable description = '';
   @observable startTime = null;
   @observable endTime = null;
 
+  @action set_id(event_id) {
+    if (this._id !== event_id) {
+      this.resetEvent();
+      this._id = event_id;
+    }
+  }
+
+  @action resetEvent() {
+    this.title = '';
+    this.description = '';
+    this.startTime = null;
+    this.endTime = null;
+  }
+
+  @action setTitle(title) {
+    this.title = title;
+  }
+
+  @action setDescription(description) {
+    this.description = description;
+  }
+
+  @action setStartTime(startTime) {
+    this.startTime = startTime;
+  }
+
+  @action setEndTime(endTime) {
+    this.endTime = endTime;
+  }
+
+  @action submitEvent() {
+    const event = {
+      _id: this._id,
+      title: this.title,
+      description: this.description,
+      startTime: this.startTime,
+      endTime: this.endTime
+    };
+    return (this._id
+      ? calendarStore.updateEvent(event)
+      : calendarStore.createEvent(event)
+    ).catch(action(err => console.error(err)));
+  }
 }
 
 export default new Event();
