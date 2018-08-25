d7bc443017769673424bdef9c9ecdcd073bfde68
car <carmelogustavo@gmail.com>
2018-08-25 15:32:24 +0200

    feat: error handlers in create event

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import '../style/Create.css';
 import { inject, observer } from 'mobx-react';
+import {
+  formErrorHandler,
+  formRequiredFieldHandler,
+  formDatesHander
+} from '../helpers/formErrorHandler';
 
 @inject('eventStore')
 @observer
 class Create extends Component {
+  state = {
+    touched: {
+      title: false,
+      startTime: false,
+      endTime: false
+    }
+  };
+
+  handleBlur = field => e => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true }
+    });
+  };
+
   handleChangeTitle = e => this.props.eventStore.setTitle(e.target.value);
   handleChangeDescription = e =>
     this.props.eventStore.setDescription(e.target.value);
@@ -23,6 +42,22 @@ class Create extends Component {
     this.props.eventStore.resetEvent();
   };
 
+  handleFieldErrorMessage = (inputProperty, field) => {
+    return this.state.touched[inputProperty] &&
+      formRequiredFieldHandler(field) ? (
+      <p className="required">This field is required</p>
+    ) : null;
+  };
+
+  handleDatesErrorMessage = (startTime, endTime) => {
+    return this.state.touched['endTime'] &&
+      formDatesHander(startTime, endTime) ? (
+      <p className="required">
+        End date and time should be after start date and time
+      </p>
+    ) : null;
+  };
+
   render() {
     const { title, description, startTime, endTime } = this.props.eventStore;
     return (
@@ -39,7 +74,9 @@ class Create extends Component {
           type="text"
           placeholder="Title"
           onChange={this.handleChangeTitle}
+          onBlur={this.handleBlur('title')}
         />
+        {this.handleFieldErrorMessage('title', title)}
         <label>Description:</label>
         <input
           name="description"
@@ -55,18 +92,34 @@ class Create extends Component {
             value={startTime}
             type="datetime-local"
             onChange={this.handleChangeStartTime}
+            onBlur={this.handleBlur('startTime')}
           />
           <input type="checkbox" name="allDay" value="allDay" />
           <label>Todo el día</label>
         </div>
+        {this.handleFieldErrorMessage('startTime', startTime)}
+
         <label>End:</label>
         <input
           name="endTime"
           value={endTime}
           type="datetime-local"
           onChange={this.handleChangeEndTime}
+          onBlur={this.handleBlur('endTime')}
         />
-        <button onClick={this.handleSubmit}>Submit</button>
+        {this.handleFieldErrorMessage('endTime', endTime)}
+        {this.handleDatesErrorMessage(startTime, endTime)}
+        <button
+          onClick={this.handleSubmit}
+          disabled={formErrorHandler(
+            formRequiredFieldHandler(title),
+            formDatesHander(startTime, endTime),
+            formRequiredFieldHandler(startTime),
+            formRequiredFieldHandler(endTime)
+          )}
+        >
+          Submit
+        </button>
       </form>
     );
   }
--- src/helpers/formErrorHandler.js
+++ src/helpers/formErrorHandler.js
@@ -1,14 +1,14 @@
-export const formTilteHandler = (title) => {
-  if (!title) return true;
+export const formRequiredFieldHandler = field => {
+  if (!field) return true;
   return false;
-}
+};
 
 export const formDatesHander = (startTime, endTime) => {
   if (startTime > endTime) return true;
   return false;
-}
+};
 
-export const formErrorHandler = () => {
-  if (formDatesHander || formTilteHandler) return true;
-  return false;
-}
+export const formErrorHandler = (...args) => {
+  const formErrors = [...args];
+  return !formErrors.every(error => error);
+};
