55e8b83f0d8f0e56b7a7f1077de4f7702adb3a61
car <carmelogustavo@gmail.com>
2018-08-31 21:49:07 +0200

    refactor: type checking for form error Handling

--- src/helpers/formErrorHandler.js
+++ src/helpers/formErrorHandler.js
@@ -1,10 +1,10 @@
 // @flow
 
-export const formRequiredFieldHandler = field => (!field ? true : false);
+export const formRequiredFieldHandler = (field: string): boolean => (!field ? true : false);
 
-export const formDatesHander = (startTime, endTime) => startTime > endTime;
+export const formDatesHander = (startTime: string, endTime: string): boolean => startTime > endTime;
 
-export const formErrorHandler = (...args) => {
+export const formErrorHandler = (...args: Array<boolean>): boolean => {
   const formErrors = [...args];
   return formErrors.some(error => error);
 };
